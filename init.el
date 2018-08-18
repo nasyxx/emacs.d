1a4a99972692b181f9691f0a292a81e5903cddbd
Nasy <nasyxx+git@gmail.com>
2018-08-19 00:15:58 +0800

    Fixed some ligatures.

--- init.el
+++ init.el
@@ -300,11 +300,10 @@
                                 (bookmarks . 5)
                                 (projects  . 5)
                                 (agenda    . 5)
-                                (registers . 5)))
+                                (registers . 5))
+              initial-buffer-choice #'(lambda () (get-buffer "*dashboard*")))
   :config
-  (dashboard-setup-startup-hook)
-  (when (get-buffer "*dashboard*")
-    (setq initial-buffer-choice #'(lambda () (get-buffer "*dashboard*")))))
+  (dashboard-setup-startup-hook))
 
 ;; Require packages
 ;;----------------------------------------------------------------------------
@@ -1278,7 +1277,8 @@ instead."
   :init
   (define-auto-insert
     '("\\.py" . "Python Language")
-    '("#!/usr/bin/env python3\n"
+    '("Python Language"
+      "#!/usr/bin/env python3\n"
       "# -*- coding: utf-8 -*-\n"
       "\"\"\"\n"
       "Life's pathetic, have fun (\"▔□▔)/hi~♡ Nasy.\n\n"
@@ -1312,7 +1312,45 @@ instead."
       "license  : GPL-3.0+\n\n"
       "There are more things in heaven and earth, Horatio, than are dreamt.\n"
       " --  From \"Hamlet\"\n"
-      "\"\"\"\n")))
+      "\"\"\"\n"))
+
+  (define-auto-insert
+    '("\\.hs" . "Haskell Language")
+    '("Haskell Language"
+      "{-\n"
+      " Excited without bugs, have fun (\"▔□▔)/hi~♡ Nasy.\n"
+      " -----------------------------------------------\n"
+      " |             *         *\n"
+      " |                  .                .\n"
+      " |           .\n"
+      " |     *                      ,\n"
+      " |                   .\n"
+      " |\n"
+      " |                               *\n"
+      " |          |\\___/|\n"
+      " |          )    -(             .              ·\n"
+      " |         =\\ -   /=\n"
+      " |           )===(       *\n"
+      " |          /   - \\\n"
+      " |          |-    |\n"
+      " |         /   -   \\     0.|.0\n"
+      " |  NASY___\\__( (__/_____(\\=/)__+1s____________\n"
+      " |  ______|____) )______|______|______|______|_\n"
+      " |  ___|______( (____|______|______|______|____\n"
+      " |  ______|____\\_|______|______|______|______|_\n"
+      " |  ___|______|______|______|______|______|____\n"
+      " |  ______|______|______|______|______|______|_\n"
+      " |  ___|______|______|______|______|______|____\n\n"
+      "There are more things in heaven and earth, Horatio, than are dreamt.\n"
+      "   -- From \"Hamlet\"\n"
+      "--------------------------------------------------------------------------------\n\n-}\n\n"
+      "--------------------------------------------------------------------------------\n-- |\n"
+      "-- Filename   : " (file-name-nondirectory (buffer-file-name)) \n
+      "-- Project    : " (file-name-nondirectory (directory-file-name (projectile-project-root))) \n
+      "-- Author     : Nasy\n"
+      "-- License    : GPL-3.0+\n--\n"
+      "-- Maintainer : Nasy <nasyxx+haskell@gmail.com>\n"
+      "--\n--\n--\n--------------------------------------------------------------------------------\n")))
 
 
 ;; pretty
@@ -1378,49 +1416,22 @@ instead."
                                   ("sum" .      #x2211))))))
   :config
   ;; pretty python mode
-  (pretty-deactivate-groups '( :sets-relations  ;; break int in python mode
-                               :logic           ;; break for in python mode
-                               )
-                            'python-mode)
-  (pretty-add-keywords 'python-mode '(
-                                      ("->" .       #X2192)  ;; →
-                                      ;; ("def" .      #x2131)
-                                      ;; ;; ("not" .      #x2757)
-                                      ;; ("not" .      #xac)
-                                      ;; ("in" .       #x2208)
-                                      ;; ("not in" .   #x2209)
-                                      ;; ("return" .   #x27fc)
-                                      ;; ("yield" .    #x27fb)
-                                      ;; ("for" .      #x2200)
-                                      ;; ;; Extend Functions
-                                      ;; ("any" .      #x2754)
-                                      ;; ("all" .      #x2201)
-                                      ;; ("sum" .      #x2211)
-                                      ;; ("dict" .     #x1d507)
-                                      ;; ("list" .     #x2112)
-                                      ;; ("tuple" .    #x2a02)
-                                      ;; ("set" .      #x2126)
-                                      ;; ;; Base Types
-                                      ;; ("int" .      #x2124)
-                                      ;; ("float" .    #x211d)
-                                      ;; ("str" .      #x1d54a)
-                                      ;; ("True" .     #x1d54b)
-                                      ;; ("False" .    #x1d53d)
-                                      ;; ;; Extend Types
-                                      ;; ("Int" .      #x2124)
-                                      ;; ("Float" .    #x211d)
-                                      ;; ("String" .   #x1d54a)
-                                      ;; ;; Mypy
-                                      ;; ("Dict" .     #x1d507)
-                                      ;; ("List" .     #x2112)
-                                      ;; ("Tuple" .    #x2a02)
-                                      ;; ("Set" .      #x2126)
-                                      ;; ("Iterable" . #x1d50a)
-                                      ;; ("Any" .      #x2754)
-                                      ;; ("Union" .    #x22c3)
-                                      ))
+  ;; (pretty-deactivate-groups '( :sets-relations  ;; break int in python mode
+  ;;                              :logic           ;; break for in python mode
+  ;;                              )
+  ;;                           'python-mode)
+  ;; (pretty-add-keywords 'python-mode '(
+  ;;                                     ("->" .       #X2192)  ;; →
+  ;;                                     ))
+
+  (pretty-deactivate-groups
+   '(:equality :ordering :ordering-double :ordering-triple
+               :arrows :arrows-twoheaded :punctuation
+               :logic :sets))
+
   (pretty-activate-groups
-   '(:sub-and-superscripts :greek :arithmetic-nary)))
+   '(:sub-and-superscripts :greek :arithmetic-nary))
+  )
 
 
 (use-package ipretty
@@ -1435,132 +1446,123 @@ instead."
 (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")
 ;; I haven't found one statement that makes both of the above situations work, so I use both for now
 
-(defconst fira-code-font-lock-keywords-alist
-  (mapcar (lambda (regex-char-pair)
-            `(,(car regex-char-pair)
-              (0 (prog1 ()
-                   (compose-region (match-beginning 1)
-                                   (match-end 1)
-                                   ;; The first argument to concat is a string containing a literal tab
-                                   ,(concat "	" (list (decode-char 'ucs (cadr regex-char-pair)))))))))
-          '(("\\(www\\)"                   #Xe100)
-            ("[^/]\\(\\*\\*\\)[^/]"        #Xe101)
-            ("\\(\\*\\*\\*\\)"             #Xe102)
-            ("\\(\\*\\*/\\)"               #Xe103)
-            ("\\(\\*>\\)"                  #Xe104)
-            ("[^*]\\(\\*/\\)"              #Xe105)
-            ("\\(\\\\\\\\\\)"              #Xe106)
-            ("\\(\\\\\\\\\\\\\\)"          #Xe107)
-            ("\\({-\\)"                    #Xe108)
-            ;; ("\\(\\[\\]\\)"                #Xe109)
-            ("\\(::\\)"                    #Xe10a)
-            ("\\(:::\\)"                   #Xe10b)
-            ("[^=]\\(:=\\)"                #Xe10c)
-            ("\\(!!\\)"                    #Xe10d)
-            ("\\(!=\\)"                    #Xe10e)
-            ("\\(!==\\)"                   #Xe10f)
-            ("\\(-}\\)"                    #Xe110)
-            ("\\(--\\)"                    #Xe111)
-            ("\\(---\\)"                   #Xe112)
-            ("\\(-->\\)"                   #Xe113)
-            ("[^-]\\(->\\)"                #Xe114)
-            ("\\(->>\\)"                   #Xe115)
-            ("\\(-<\\)"                    #Xe116)
-            ("\\(-<<\\)"                   #Xe117)
-            ("\\(-~\\)"                    #Xe118)
-            ("\\(#{\\)"                    #Xe119)
-            ("\\(#\\[\\)"                  #Xe11a)
-            ("\\(##\\)"                    #Xe11b)
-            ("\\(###\\)"                   #Xe11c)
-            ("\\(####\\)"                  #Xe11d)
-            ("\\(#(\\)"                    #Xe11e)
-            ("\\(#\\?\\)"                  #Xe11f)
-            ("\\(#_\\)"                    #Xe120)
-            ("\\(#_(\\)"                   #Xe121)
-            ("\\(\\.-\\)"                  #Xe122)
-            ("\\(\\.=\\)"                  #Xe123)
-            ("\\(\\.\\.\\)"                #Xe124)
-            ("\\(\\.\\.<\\)"               #Xe125)
-            ("\\(\\.\\.\\.\\)"             #Xe126)
-            ("\\(\\?=\\)"                  #Xe127)
-            ("\\(\\?\\?\\)"                #Xe128)
-            ("\\(;;\\)"                    #Xe129)
-            ("\\(/\\*\\)"                  #Xe12a)
-            ("\\(/\\*\\*\\)"               #Xe12b)
-            ("\\(/=\\)"                    #Xe12c)
-            ("\\(/==\\)"                   #Xe12d)
-            ("\\(/>\\)"                    #Xe12e)
-            ("\\(//\\)"                    #Xe12f)
-            ("\\(///\\)"                   #Xe130)
-            ("\\(&&\\)"                    #Xe131)
-            ("\\(||\\)"                    #Xe132)
-            ("\\(||=\\)"                   #Xe133)
-            ("[^|]\\(|=\\)"                #Xe134)
-            ("\\(|>\\)"                    #Xe135)
-            ("\\(\\^=\\)"                  #Xe136)
-            ("\\(\\$>\\)"                  #Xe137)
-            ("\\(\\+\\+\\)"                #Xe138)
-            ("\\(\\+\\+\\+\\)"             #Xe139)
-            ("\\(\\+>\\)"                  #Xe13a)
-            ("\\(=:=\\)"                   #Xe13b)
-            ("[^!/]\\(==\\)[^>]"           #Xe13c)
-            ("\\(===\\)"                   #Xe13d)
-            ("\\(==>\\)"                   #Xe13e)
-            ("[^=]\\(=>\\)"                #Xe13f)
-            ("\\(=>>\\)"                   #Xe140)
-            ("\\(<=\\)"                    #Xe141)
-            ("\\(=<<\\)"                   #Xe142)
-            ("\\(=/=\\)"                   #Xe143)
-            ("\\(>-\\)"                    #Xe144)
-            ("\\(>=\\)"                    #Xe145)
-            ("\\(>=>\\)"                   #Xe146)
-            ("[^-=]\\(>>\\)"               #Xe147)
-            ("\\(>>-\\)"                   #Xe148)
-            ("\\(>>=\\)"                   #Xe149)
-            ("\\(>>>\\)"                   #Xe14a)
-            ("\\(<\\*\\)"                  #Xe14b)
-            ("\\(<\\*>\\)"                 #Xe14c)
-            ("\\(<|\\)"                    #Xe14d)
-            ("\\(<|>\\)"                   #Xe14e)
-            ("\\(<\\$\\)"                  #Xe14f)
-            ("\\(<\\$>\\)"                 #Xe150)
-            ("\\(<!--\\)"                  #Xe151)
-            ("\\(<-\\)"                    #Xe152)
-            ("\\(<--\\)"                   #Xe153)
-            ("\\(<->\\)"                   #Xe154)
-            ("\\(<\\+\\)"                  #Xe155)
-            ("\\(<\\+>\\)"                 #Xe156)
-            ("\\(<=\\)"                    #Xe157)
-            ("\\(<==\\)"                   #Xe158)
-            ("\\(<=>\\)"                   #Xe159)
-            ("\\(<=<\\)"                   #Xe15a)
-            ("\\(<>\\)"                    #Xe15b)
-            ("[^-=]\\(<<\\)"               #Xe15c)
-            ("\\(<<-\\)"                   #Xe15d)
-            ("\\(<<=\\)"                   #Xe15e)
-            ("\\(<<<\\)"                   #Xe15f)
-            ("\\(<~\\)"                    #Xe160)
-            ("\\(<~~\\)"                   #Xe161)
-            ("\\(</\\)"                    #Xe162)
-            ("\\(</>\\)"                   #Xe163)
-            ("\\(~@\\)"                    #Xe164)
-            ("\\(~-\\)"                    #Xe165)
-            ("\\(~=\\)"                    #Xe166)
-            ("\\(~>\\)"                    #Xe167)
-            ("[^<]\\(~~\\)"                #Xe168)
-            ("\\(~~>\\)"                   #Xe169)
-            ("\\(%%\\)"                    #Xe16a)
-           ;; ("\\(x\\)"                   #Xe16b) This ended up being hard to do properly so i'm leaving it out.
-            ("[^:=]\\(:\\)[^:=]"           #Xe16c)
-            ("[^\\+<>]\\(\\+\\)[^\\+<>]"   #Xe16d)
-            ("[^\\*/<>]\\(\\*\\)[^\\*/<>]" #Xe16f))))
+(defun pretty-fonts-set-fontsets (CODE-FONT-ALIST)
+  "Utility to associate many unicode points with specified fonts."
+  (--each CODE-FONT-ALIST
+    (-let (((font . codes) it))
+      (--each codes
+        (set-fontset-font nil `(,it . ,it) font)
+        (set-fontset-font t `(,it . ,it) font)))))
 
-(defun add-fira-code-symbol-keywords ()
-  (font-lock-add-keywords nil fira-code-font-lock-keywords-alist))
+(defun pretty-fonts--add-kwds (FONT-LOCK-ALIST)
+  "Exploits `font-lock-add-keywords' to apply regex-unicode replacements."
+  (font-lock-add-keywords
+   nil (--map (-let (((rgx uni-point) it))
+               `(,rgx (0 (progn
+                           (compose-region
+                            (match-beginning 1) (match-end 1)
+                            ,(concat "\t" (list uni-point)))
+                           nil))))
+             FONT-LOCK-ALIST)))
 
-;; (add-hook 'prog-mode-hook
-;;           #'add-fira-code-symbol-keywords)
+(defmacro pretty-fonts-set-kwds (FONT-LOCK-HOOKS-ALIST)
+  "Set regex-unicode replacements to many modes."
+  `(--each ,FONT-LOCK-HOOKS-ALIST
+     (-let (((font-locks . mode-hooks) it))
+       (--each mode-hooks
+         (add-hook it (-partial 'pretty-fonts--add-kwds
+                                (symbol-value font-locks)))))))
 
+(defconst pretty-fonts-fira-font
+  '(;; OPERATORS
+    ;; Pipes
+    ("\\(<|\\)" #Xe14d) ("\\(<>\\)" #Xe15b) ("\\(<|>\\)" #Xe14e) ("\\(|>\\)" #Xe135)
+
+    ;; Brackets
+    ("\\(<\\*\\)" #Xe14b) ("\\(<\\*>\\)" #Xe14c) ("\\(\\*>\\)" #Xe104)
+    ("\\(<\\$\\)" #Xe14f) ("\\(<\\$>\\)" #Xe150) ("\\(\\$>\\)" #Xe137)
+    ("\\(<\\+\\)" #Xe155) ("\\(<\\+>\\)" #Xe156) ("\\(\\+>\\)" #Xe13a)
+
+    ;; Equality
+    ("\\(!=\\)" #Xe10e) ("\\(!==\\)"         #Xe10f) ("\\(=/=\\)" #Xe143)
+    ("\\(/=\\)" #Xe12c) ("\\(/==\\)"         #Xe12d)
+    ("\\(===\\)"#Xe13d) ("[^!/]\\(==\\)[^>]" #Xe13c)
+
+    ;; Equality Special
+    ("\\(||=\\)"  #Xe133) ("[^|]\\(|=\\)" #Xe134)
+    ("\\(~=\\)"   #Xe166)
+    ("\\(\\^=\\)" #Xe136)
+    ("\\(=:=\\)"  #Xe13b)
+
+    ;; Comparisons
+    ("\\(<=\\)" #Xe141) ("\\(>=\\)" #Xe145)
+    ("\\(</\\)" #Xe162) ("\\(</>\\)" #Xe163)
+
+    ;; Shifts
+    ("[^-=]\\(>>\\)" #Xe147) ("\\(>>>\\)" #Xe14a)
+    ("[^-=]\\(<<\\)" #Xe15c) ("\\(<<<\\)" #Xe15f)
+
+    ;; Dots
+    ("\\(\\.-\\)"    #Xe122) ("\\(\\.=\\)" #Xe123)
+    ("\\(\\.\\.<\\)" #Xe125)
+
+    ;; Hashes
+    ("\\(#{\\)"  #Xe119) ("\\(#(\\)"   #Xe11e) ("\\(#_\\)"   #Xe120)
+    ("\\(#_(\\)" #Xe121) ("\\(#\\?\\)" #Xe11f) ("\\(#\\[\\)" #Xe11a)
+
+    ;; REPEATED CHARACTERS
+    ;; 2-Repeats
+    ("\\(||\\)" #Xe132)
+    ("\\(!!\\)" #Xe10d)
+    ("\\(%%\\)" #Xe16a)
+    ("\\(&&\\)" #Xe131)
+
+    ;; 2+3-Repeats
+    ("\\(##\\)"       #Xe11b) ("\\(###\\)"          #Xe11c) ("\\(####\\)" #Xe11d)
+    ("\\(--\\)"       #Xe111) ("\\(---\\)"          #Xe112)
+    ("\\({-\\)"       #Xe108) ("\\(-}\\)"           #Xe110)
+    ("\\(\\\\\\\\\\)" #Xe106) ("\\(\\\\\\\\\\\\\\)" #Xe107)
+    ("\\(\\.\\.\\)"   #Xe124) ("\\(\\.\\.\\.\\)"    #Xe126)
+    ("\\(\\+\\+\\)"   #Xe138) ("\\(\\+\\+\\+\\)"    #Xe139)
+    ("\\(//\\)"       #Xe12f) ("\\(///\\)"          #Xe130)
+    ("\\(::\\)"       #Xe10a) ("\\(:::\\)"          #Xe10b)
+
+    ;; ARROWS
+    ;; Direct
+    ("[^-]\\(->\\)" #Xe114) ("[^=]\\(=>\\)" #Xe13f)
+    ("\\(<-\\)"     #Xe152)
+    ("\\(-->\\)"    #Xe113) ("\\(->>\\)"    #Xe115)
+    ("\\(==>\\)"    #Xe13e) ("\\(=>>\\)"    #Xe140)
+    ("\\(<--\\)"    #Xe153) ("\\(<<-\\)"    #Xe15d)
+    ("\\(<==\\)"    #Xe158) ("\\(<<=\\)"    #Xe15e)
+    ("\\(<->\\)"    #Xe154) ("\\(<=>\\)"    #Xe159)
+
+    ;; Branches
+    ("\\(-<\\)"  #Xe116) ("\\(-<<\\)" #Xe117)
+    ("\\(>-\\)"  #Xe144) ("\\(>>-\\)" #Xe148)
+    ("\\(=<<\\)" #Xe142) ("\\(>>=\\)" #Xe149)
+    ("\\(>=>\\)" #Xe146) ("\\(<=<\\)" #Xe15a)
+
+    ;; Squiggly
+    ("\\(<~\\)" #Xe160) ("\\(<~~\\)" #Xe161)
+    ("\\(~>\\)" #Xe167) ("\\(~~>\\)" #Xe169)
+    ("\\(-~\\)" #Xe118) ("\\(~-\\)"  #Xe165)
+
+    ;; MISC
+    ("\\(www\\)"                   #Xe100)
+    ("\\(<!--\\)"                  #Xe151)
+    ("\\(~@\\)"                    #Xe164)
+    ("[^<]\\(~~\\)"                #Xe168)
+    ("\\(\\?=\\)"                  #Xe127)
+    ("[^=]\\(:=\\)"                #Xe10c)
+    ("\\(/>\\)"                    #Xe12e)
+    ("[^\\+<>]\\(\\+\\)[^\\+<>]"   #Xe16d)
+    ("[^:=]\\(:\\)[^:=]"           #Xe16c)
+    ("\\(<=\\)"                    #Xe157))
+  "Fira font ligatures and their regexes")
+
+(pretty-fonts-set-kwds
+ '((pretty-fonts-fira-font prog-mode-hook org-mode-hook)))
 
 ;; Languages
 ;;----------------------------------------------------------------------------
