8df18d58e0bd601f0294175c2e19dc76dd68e968
Du Yizhuo <dyzdyz010@sina.com>
2018-08-20 16:41:55 +0800

    macOS-specific keybindings

--- config.el
+++ config.el
@@ -41,15 +41,16 @@
 
 ;; Cursor Movement
 
-(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
-(global-set-key (kbd "s-<right>") 'move-end-of-line)
-(global-set-key (kbd "s-<up>") 'beginning-of-buffer)
-(global-set-key (kbd "s-<down>") 'end-of-buffer)
+(when *is-a-mac*  ;; init.el:16 (defconst *is-a-mac* (eq system-type 'darwin))
+  (global-set-key (kbd "S-<left>") 'move-beginning-of-line)
+  (global-set-key (kbd "S-<right>") 'move-end-of-line)
+  (global-set-key (kbd "S-<up>") 'beginning-of-buffer)
+  (global-set-key (kbd "S-<down>") 'end-of-buffer))
 
 
 ;; Text Operations
 
-(global-set-key (kbd "s-<backspace>") (lambda ()
+(global-set-key (kbd "S-<backspace>") (lambda ()
                                         (interactive)
                                         (kill-line 0)))
 
