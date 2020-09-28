ae6286628056aa61135b53ecce59232f26db2f4d
Nasy <nasyxx+git@gmail.com>
2020-09-28 13:34:22 -0400

    Change use-package to leaf and adjust structure.

--- Makefile
+++ Makefile
@@ -3,8 +3,62 @@ EMACS ?= emacs
 all: help
 .PHONY: all
 
-## Generate init.el from README.org
-generate:
+
+## Generate init.el from literate-config/README.org
+init.el: literate-config/README.org
+	@echo "Generate init.el from literate-config/README.org"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "init")'
+
+
+## Generate config/nasy-bootstrap.el from literate-config/bootstrap
+config/nasy-bootstrap.el: $(wildcard literate-config/bootstrap/*.org)
+	@echo "Generate config/nasy-bootstrap.el from literate-config/bootstrap/README.org"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "bootstrap")'
+
+
+## Generate config/core from literate-config/core
+config/core/nasy-core.el: $(wildcard literate-config/core/*.org)
+	@echo "Generate config/core from literate-config/core"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "core")'
+
+
+## Generate config/editor from literate-config/editor
+config/editor/nasy-editor.el: $(wildcard literate-config/editor/*.org)
+	@echo "Generate config/editor from literate-config/editor"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "editor")'
+
+
+## Generate config/tools from literate-config/tools
+config/tools/nasy-tools.el: $(wildcard literate-config/tools/*.org)
+	@echo "Generate config/tools from literate-config/tools"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "tools")'
+
+
+## Generate config/langs from literate-config/langs
+config/langs/nasy-langs.el: $(wildcard literate-config/langs/*.org)
+	@echo "Generate config/langs from literate-config/langs"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "langs")'
+
+
+## Generate config/ui from literate-config/org
+config/org/nasy-org.el: $(wildcard literate-config/org/*.org)
+	@echo "Generate config/org from literate-config/org"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "org")'
+
+
+## Generate config/ui from literate-config/ui
+config/ui/nasy-ui.el: $(wildcard literate-config/ui/*.org)
+	@echo "Generate config/ui from literate-config/ui"
+	@$(EMACS) -Q --batch -l export.el --eval '(org-publish "ui")'
+
+
+## Generate all
+generate: init.el config/nasy-bootstrap.el config/core/nasy-core.el config/editor/nasy-editor.el config/tools/nasy-tools.el config/langs/nasy-langs.el config/ui/nasy-ui.el config/org/nasy-org.el
+.PHONY: generate
+
+
+## Old Generate init.el from README.org
+old-generate:
 	@echo "Generate init.el from README.org and literate-config.org."
 	@mkdir -p custom && \
 		$(EMACS) -Q --batch --find-file "README.org" -f "org-babel-tangle"
@@ -13,7 +67,7 @@ generate:
 		rm -rf "literate-config.org.org" "literate-config.org.org~"
 	@$(EMACS) -Q --batch --find-file "extra/README.org" -f "org-babel-tangle"
 	@echo "If you want to customize, you can simply change/create custom/user-config.el"
-.PHONY: generate
+.PHONY: old-generate
 
 ## Clean build (straight/)
 clean-build:
---
+++ export.el
@@ -0,0 +1,109 @@
+;;; export.el --- Nasy's Emacs configuration export script.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Nasy
+
+;; Author: Nasy <nasyxx@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Nasy's Emacs configuration export script.
+
+;;; Code:
+
+(require 'ox-publish)
+
+(setq org-publish-timestamp-directory
+      (expand-file-name "var/org/timestamps/" user-emacs-directory)
+      nasy/publish-base "~/.emacs.d/config")
+
+(setq org-publish-project-alist
+      `(("pre-bootstrap"
+         :base-directory "~/.emacs.d/literate-config/bootstrap/"
+         :base-extension "org"
+         :recursive nil
+         :exclude ".*"
+         :include ("README.org")
+         :publishing-directory ,(expand-file-name "bootstrap" nasy/publish-base)
+         :publishing-function org-org-publish-to-org)
+        ("init"
+         :base-directory "~/.emacs.d/literate-config/"
+         :base-extension "org"
+         :recursive nil
+         :exclude ".*"
+         :include ("README.org")
+         :publishing-directory ,(expand-file-name "../" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("bootstrap"
+         :base-directory "~/.emacs.d/literate-config/bootstrap/"
+         :base-extension "org"
+         :recursive nil
+         :exclude ".*"
+         :include ("README.org")
+         :publishing-directory ,(expand-file-name nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("core"
+         :base-directory "~/.emacs.d/literate-config/core/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude "keydef"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "core" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("editor"
+         :base-directory "~/.emacs.d/literate-config/editor/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude ".*"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "editor" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("tools"
+         :base-directory "~/.emacs.d/literate-config/tools/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude ".*"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "tools" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("langs"
+         :base-directory "~/.emacs.d/literate-config/langs/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude ".*"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "langs" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("org"
+         :base-directory "~/.emacs.d/literate-config/org/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude ".*"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "org" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("ui"
+         :base-directory "~/.emacs.d/literate-config/ui/"
+         :base-extension "org"
+         :recursive nil
+         ;; :exclude ".*"
+         ;; :include ("README.org")
+         :publishing-directory ,(expand-file-name "ui" nasy/publish-base)
+         :publishing-function org-babel-tangle-publish)
+        ("all"
+         :components ("init" "bootstrap" "core" "editor" "tools" "langs" "org" "ui"))))
+
+(provide 'export)
+;;; export.el ends here
